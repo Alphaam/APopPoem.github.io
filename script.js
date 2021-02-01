f804f8b982e5e0a6caac2706eaad95ce6e288780
Alphaam <58554818+Alphaam@users.noreply.github.com>
2021-02-01 04:56:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    
+    $("#About-Image").fadeOut(900);
+    $("#button1").fadeOut(900);
+   
+
+    $("#First-verse").fadeIn(1000);
+    
+    $("#button3").fadeIn(1000);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
